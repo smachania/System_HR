b0985d5af06c7b29af9c90139871dfe290a82f21
Julia Janusz <julia.zofia.janusz@gmail.com>
2026-01-10 23:58:22 +0100

    Dodanie klas B2BContract oraz InternshipContract jako różne typy umów z bazą w klasie Contract

---
+++ System_HR/System_HR/B2BContract.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace System_hr.System_HR
+{
+	public class B2BContract : Contract
+	{
+        decimal hourlyRate;
+        int monthlyHours;
+
+       
+        public decimal HourlyRate { get; private set; }
+        public int MonthlyHours { get; private set; }
+       
+        
+        public B2BContract(DateTime startDate, decimal hourlyRate, int monthlyHours)
+            : base(startDate, hourlyRate * monthlyHours)
+        {
+            HourlyRate = hourlyRate;
+            MonthlyHours = monthlyHours;
+        }
+        
+        
+        public override decimal CalculateSalary()
+        {
+            return HourlyRate * MonthlyHours;
+        }
+        
+        
+        public override string GetContractType()
+        {
+            return "B2B Contract";
+        }
+    }
+}
