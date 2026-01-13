9d6c5bdfe6a9d447d429d3f64df410e1a90823ab
Julia Janusz <julia.zofia.janusz@gmail.com>
2026-01-13 23:47:58 +0100

    Dodanie pola vatRate oraz metody obliczania płacy brutto: CalculateTotalSalary()

--- System_HR/System_HR/B2BContract.cs
+++ System_HR/System_HR/B2BContract.cs
@@ -7,13 +7,15 @@ namespace System_hr.System_HR
 	{
         decimal hourlyRate;
         int monthlyHours;
+        decimal vatRate = 0.23m; //stawka VAT domyślna 23%
 
-       
         public decimal HourlyRate { get; private set; }
         public int MonthlyHours { get; private set; }
-       
-        
-        public B2BContract(DateTime startDate, decimal hourlyRate, int monthlyHours)
+        public decimal VatRate { get; private set; } 
+
+
+        public B2BContract(DateTime startDate, decimal hourlyRate, int monthlyHours,
+            decimal vatRate = 0.23m)
             : base(startDate, hourlyRate * monthlyHours)
         {
             HourlyRate = hourlyRate;
@@ -21,12 +23,24 @@ namespace System_hr.System_HR
         }
         
         
-        public override decimal CalculateSalary()
+        public override decimal CalculateSalary() //płaca netto
         {
             return HourlyRate * MonthlyHours;
         }
+
+
+        public decimal CalculateVat()
+        {
+            return CalculateSalary() * VatRate;
+        }
         
-        
+
+        public decimal CalculateTotalSalary()  //płaca brutto
+        {
+            return CalculateSalary() + CalculateVat();
+        }
+
+
         public override string GetContractType()
         {
             return "B2B Contract";
