4fd5429f140b85c315596b9a3bc7cd603e6aa80c
Anna Smach <asmach@student.agh.edu.pl>
2026-01-14 22:47:30 +0100

    Rozpoczęcie pracy nad testowaniem metod oraz dodanie metody do zliczania pracowników działu oraz nadpisanie potrzebnych metod ToString().

---
+++ System_HR/System_HR/Department.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System_hr.System_HR;
+
+namespace System_hr
+{
+    class Department
+    {
+        string name;
+        Employee manager;
+        List<Employee> employees;
+
+        public string Name { get; private set; }
+        public Employee Manager { get; private set; }
+        public List<Employee> Employess { get; }
+
+        public Department(string name)
+        {
+            this.Name = name;
+            employees = new List<Employee>();
+        }
+        public void AddEmployee(Employee emp)
+        {
+            if (emp is null) return;
+            foreach(var e in employees)
+            {
+                if (e.Equals(emp)) //wykorzystanie funkcji Equals
+                {
+                    return;
+                }
+            }
+            employees.Add(emp);
+        }
+        public void SetManager(Employee manager)
+        {
+            if(manager is null) { return; }
+            if (manager.IsActive)
+            {
+                this.Manager = manager;
+                AddEmployee(manager);
+            }
+        }
+        //wykorzystanie metody Equals z klasy Empolyee
+        public bool RemoveEmployee(Employee emp)
+        {
+            if (emp is null) return false;
+            bool removed = employees.Remove(emp);
+            if(Manager!= null && Manager.Equals(emp))
+            {
+                Manager = null;
+            }
+            return removed;
+        }
+        public int NumberOfEmployeesByDepart()
+        {
+            return employees.Count();
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Department name: {Name} | Employees Number: {NumberOfEmployeesByDepart()}");
+            foreach(var employ in employees)
+            {
+                sb.AppendLine(employ.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
--- System_HR/System_HR/Program.cs
+++ System_HR/System_HR/Program.cs
@@ -1,4 +1,5 @@
-﻿using System_hr.System_HR;
+﻿using System_hr;
+using System_hr.System_HR;
 
 internal class Program
 {
@@ -8,7 +9,47 @@ internal class Program
     }
     static void EmployeeTest()
     {
-        Employee e1 = new("Jan", "Mucha", EnumPlec.M, "99876756333", new DateTime(2023, 10, 15));
-        Console.WriteLine(e1);
+        Employee e1 = new("Robert", "Lewandowski", EnumPlec.M, "82010112345", new DateTime(2015, 5, 10));
+        Employee e2 = new("Anna", "Zielińska", EnumPlec.K, "88051255667", new DateTime(2018, 3, 20));
+        Employee e3 = new("Maria", "Mazur", EnumPlec.K, "03251211223", new DateTime(2023, 12, 01));
+
+        Employee e4 = new("Karolina", "Krawczyk", EnumPlec.K, "90060677889", new DateTime(2019, 1, 15));
+        Employee e5 = new("Julia", "Nowakowska", EnumPlec.K, "95022833441", new DateTime(2021, 8, 10));
+
+        Employee e6 = new("Barbara", "Jankowska", EnumPlec.K, "75081522334", new DateTime(2010, 4, 12));
+        Employee e7 = new("Jan", "Wójcik", EnumPlec.M, "81041266778", new DateTime(2016, 2, 28));
+
+        Employee e8 = new("Michał", "Wiśniewski", EnumPlec.M, "89030311223", new DateTime(2018, 11, 12));
+        Employee e9 = new("Adam", "Dudek", EnumPlec.M, "96070755443", new DateTime(2022, 05, 20));
+        e1.ChangeContract(new B2BContract(new DateTime(2024, 1, 1), 200m, 160));
+        e2.ChangeContract(new B2BContract(new DateTime(2024, 1, 1), 160m, 160));
+        e3.ChangeContract(new InternshipContract(new DateTime(2023, 12, 1), "Politechnika", 3, true));
+        e4.ChangeContract(new EmployeeContract(new DateTime(2024, 1, 1), 9000m, 2000m));
+        e5.ChangeContract(new EmployeeContract(new DateTime(2024, 1, 1), 6000m, 500m, 10));
+        e6.ChangeContract(new EmployeeContract(new DateTime(2024, 1, 1), 11000m));
+        e7.ChangeContract(new B2BContract(new DateTime(2024, 1, 1), 100m, 160));
+        e8.ChangeContract(new EmployeeContract(new DateTime(2024, 1, 1), 7500m));
+        e9.ChangeContract(new EmployeeContract(new DateTime(2024, 1, 1), 4500m, 200m, 25));
+        //Console.WriteLine(e6);
+        Department it = new Department("Software development");
+        it.SetManager(e1);
+        it.AddEmployee(e2);
+        it.AddEmployee(e3);
+        //Console.WriteLine(e2);
+        //Console.WriteLine($"Pensja netto: {e2.Contract.CalculateSalary()}");
+        Department marketing = new Department("Marketing & Sales");
+        marketing.SetManager(e4);
+        marketing.AddEmployee(e5);
+
+        Department admin = new Department("Administration");
+        admin.SetManager(e6);
+        admin.AddEmployee(e7);
+
+        Department services = new Department("Customer Service");
+        services.SetManager(e8);
+        services.AddEmployee(e9);
+
+        Console.WriteLine(it);
+
     }
 }
