2aa697eb734fd3511746cdb6fc22a92c51247f98
fszkamruk <fszkamruk@student.agh.edu.pl>
2026-01-21 15:40:01 +0100

    Szkielet GUI, główny panel, okno Osoby, okno Urlopu, okno ze Szczegółami i okno z Raportem

---
+++ System_hr_GUI/HRPanelWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System_hr;
+using System_hr.System_HR;
+
+namespace System_hr_GUI
+{
+    public partial class HRPanelWindow : Window
+    {
+        public HRPanelWindow()
+        {
+            InitializeComponent();
+        }
+        //LOGIIKI PRZYCISKOW
+        private void MenuWczytaj_Click(object sender, RoutedEventArgs e) { }
+        private void MenuZapisz_Click(object sender, RoutedEventArgs e) { }
+        private void BtnDodaj_Click(object sender, RoutedEventArgs e) { }
+        private void BtnEdytuj_Click(object sender, RoutedEventArgs e) { }
+        private void BtnZwolnij_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgPracownicy.SelectedItem is Employee wybrany)
+            {
+                wybrany.Terminate();
+                dgPracownicy.Items.Refresh();
+            }
+        }
+        private void BtnSzczegoly_Click(object sender, RoutedEventArgs e) { }
+    }
+}
---
+++ System_hr_GUI/OsobaWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System_hr.System_HR;
+
+namespace System_hr_GUI
+{
+    public partial class OsobaWindow : Window
+    {
+        public Employee NowyPracownik { get; private set; }
+
+        public OsobaWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Metoda do czyszczenia pola tekstowego z bledami
+        private void ResetujBledy()
+        {
+            lblError.Text = "";
+        }
+    }
+}
